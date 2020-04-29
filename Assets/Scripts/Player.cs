25caef31696d07804e74fee2583a487b84c422be
Siddharth Pawar <siddhartha.pawar17@gmail.com>
2020-04-29 12:51:34 +0530

    Player Movable

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5.0f;
+    [SerializeField] float moveSpeed = 50.0f;
+
+    Rigidbody2D myRigidbody = null;
+    Animator animator = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
+    }
+
+    private void Run()
+    {
+        float xSpeed = Input.GetAxis("Horizontal") * moveSpeed;
+        Vector2 playerVelocity = new Vector2(xSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+
+        if (Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon) {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1.0f);
+        }
+
+        animator.SetBool("IsRunning", myRigidbody.velocity.magnitude > 0);
+
     }
 }
